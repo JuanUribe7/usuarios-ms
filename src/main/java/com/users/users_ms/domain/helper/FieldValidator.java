5b0ef3d68eb2791cba4a91c91df5adce6df8a1a5
JuanUribe7 <uribej030@gmail.com>
2025-05-12 04:16:41 -0500

    arregle errores sonarlint

--- src/main/java/com/users/users_ms/domain/helper/FieldValidator.java
+++ src/main/java/com/users/users_ms/domain/helper/FieldValidator.java
@@ -11,30 +11,28 @@ public class FieldValidator {
     }
 
     public static void validateNotBlank(String field, String errorMessage) {
-        if (field == null || field.isBlank()) {
-            throw new InvalidFieldException(errorMessage) {
-            };
+        if (field == null || field.isBlank())
+            throw new InvalidFieldException(errorMessage) ;
+
         }
-    }
+
 
     public static void validatePattern(String field, String regex, String errorMessage) {
         if (!field.matches(regex)) {
-            throw new InvalidFieldException(errorMessage) {
-            };
+            throw new InvalidFieldException(errorMessage);
         }
     }
 
     public static void validateMaxLength(String field, int maxLength, String errorMessage) {
         if (field.length() > maxLength) {
-            throw new InvalidFieldException(errorMessage) {
-            };
+            throw new InvalidFieldException(errorMessage);
+
         }
     }
 
     public static void validateMinLength(String field, int minLength, String errorMessage) {
         if (field.length() < minLength) {
-            throw new InvalidFieldException(errorMessage) {
-            };
+            throw new InvalidFieldException(errorMessage);
         }
     }
 }
